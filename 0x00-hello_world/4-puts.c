d01b5d8d71ccaf0c5bf836847f935f210f5b8ed7
Emmanuel-Essiet <elninovibez@gmail.com>
2022-10-14 09:35:59 -0700

    task004

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
-/*writing a string in c*/
 #include <stdio.h>
+
+/**
+*main - this is a main function
+*description: this is the entry point of the program
+*
+*Return: returns zero if unsuccessful
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
-} 
+}
