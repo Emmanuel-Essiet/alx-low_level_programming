309afe0086b759933590ca00d86ea7d409daed9c
Emmanuel-Essiet <elninovibez@gmail.com>
2022-10-14 10:05:25 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - this is a main function
+*Description: this is the entry point
+*
+*Return: return zero if successful
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
