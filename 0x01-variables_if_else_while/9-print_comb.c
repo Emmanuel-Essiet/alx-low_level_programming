209be08ee4d9d3e429e2f921230a117f7b5ce54b
Emmanuel-Essiet <elninovibez@gmail.com>
2022-10-17 18:18:04 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: Always 0 (successful)
+  */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		putchar(ch);
+		if (ch != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
