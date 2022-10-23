a2c4f8b4693eb72e8c7c2fe644025b3e0bcf11c7
Emmanuel-Essiet <elninovibez@gmail.com>
2022-10-23 16:22:18 -0700

    task 0003

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,21 @@
 #include "main.h"
-
 /**
-*print_alphabet_x10  -  entry point
-*/
+  * print_alphabet_x10 - Entry point
+  */
 
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	char letter;
+	int a;
 
-	while (count++ <= 9)
+	int count = 0;
+
+	while (count < 10)
 	{
-		for (letter = 'a'; letter = 'z'; letter++)
-		_putchar(letter);
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		count++;
 		_putchar('\n');
 	}
 }
