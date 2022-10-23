a3bf70ae02e5c1bf8f2edec4cadbb919710976b3
Emmanuel-Essiet <elninovibez@gmail.com>
2022-10-23 15:41:48 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
-/*
-*print_alphabet_x10 - prints alphabets in lower case x10
+/**
+*print_alphabet_x10  -  entry point
 */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
